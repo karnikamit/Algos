086c0104a57157e06e5c2e28325630bf36a7916a
Amit Karnik <amit.karnik@edgenetworks.in>
2015-06-19 18:40:04 +0530

    add doc string

--- create_dict.py
+++ create_dict.py
@@ -5,6 +5,17 @@ b1 = ['a', 'b', 'c', 'd', 'e']
 
 
 def create_dict(a, b):
+    """
+
+    :param a:
+    :param b:
+    :return:this function creates a dictionary from given two lists,
+    it prepares a dict by considering the length of the lists.
+    it prepares the dict only to the items present in the 1st list 'a'.
+    If length of list 'b' is less than the length of list 'a',
+    then it appends 'None' to 'b' to make its length equal to the length of list 'a'.
+
+    """
     d = dict()
     if len(a)-len(b) != 0:
         for i in range(len(a)-len(b)):
