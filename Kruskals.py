287dc493cb3f3d9ecddf6909599e15591f20507a
Amit Karnik <amit.karnik@edgenetworks.in>
2015-06-21 00:26:42 +0530

    still in a building stage

--- Kruskals.py
+++ Kruskals.py
@@ -2,16 +2,16 @@ __author__ = 'karnikamit'
 from operator import itemgetter
 
 graph = {
-        'vertices': ['1', '2', '3', '4', '5', '6'],
-        'edges':[{'edge_w': ['1', '2'], 'weight': 05,},
-                 {'edge_w': ['1', '3'], 'weight': 10},
-                 {'edge_w': ['2', '4'], 'weight': 45},
-                 {'edge_w': ['2', '5'], 'weight': 55},
-                 {'edge_w': ['2', '6'], 'weight': 20},
-                 {'edge_w': ['3', '6'], 'weight': 25},
-                 {'edge_w': ['4', '5'], 'weight': 35},
-                 {'edge_w': ['4', '6'], 'weight': 50},
-                 {'edge_w': ['5', '6'], 'weight': 45}
+        'vertices': ['A', 'B', 'C', 'D', 'E', 'F'],
+        'edges':[{'edge_w': ['A', 'B'], 'weight': 05,},
+                 {'edge_w': ['A', 'C'], 'weight': 10},
+                 {'edge_w': ['B', 'D'], 'weight': 45},
+                 {'edge_w': ['B', 'E'], 'weight': 55},
+                 {'edge_w': ['B', 'F'], 'weight': 20},
+                 {'edge_w': ['C', 'F'], 'weight': 25},
+                 {'edge_w': ['D', 'E'], 'weight': 35},
+                 {'edge_w': ['D', 'F'], 'weight': 50},
+                 {'edge_w': ['E', 'F'], 'weight': 45}
                  ]
         }
 
@@ -32,8 +32,25 @@ class Kruskals:
         self.graph = graph
 
     def find_mst(self):
-        new = self.graph_sort()
+        # Firstly Edges arranged in increasing order of their weights
+        new_graph = sorted(self.graph['edges'], key=itemgetter('weight'), reverse=False)
+        visit = self.visited()
+        total = set()
+        for i in new_graph:
+            edges = i['edge_w']
+            for j in edges:
+                if visit[j] == 'No':
+                    visit[j] = 'Yes'
+                    total.add(i['weight'])
+        return sum(total), 'visited', visit
 
-    def graph_sort(self):
-        return sorted(self.graph['edges'], key=itemgetter('weight'), reverse=False)
+    def visited(self):
+        vertices = self.graph['vertices']
+        visit = dict()
+        for i in vertices:      # initialize all vertices as unvisited
+            visit[i] = 'No'
+        return visit
 
+
+k = Kruskals(graph)
+print k.find_mst()
