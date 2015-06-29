0fb61694d98878edef92ec90d399d1d228fe7790
Amit Karnik <amit.karnik@edgenetworks.in>
2015-06-29 09:32:09 +0530

    Linear search

--- Linear_search.py
+++ Linear_search.py
@@ -8,10 +8,17 @@ def linear_search(item, a_list):
     :param a_list: list of items
     :return: the item with the given properties if present in the list
     """
+    response = {"response": "failure", "Msg": "item Not found!"}
     position = 0
     flag = False
     while not flag and position < len(a_list):
         if item == a_list[position]:
-            return "Item found at index " + str(a_list.index(item))
+            response["response"] = "success"
+            response['Msg'] = "item found at index " + str(position)
         position += 1
-    return "Item not found"
+    return response
+
+if __author__ == 'karnikamit':
+    my_list = range(100)
+    item = 10
+    print linear_search(item, my_list)
