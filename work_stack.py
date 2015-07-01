4d9f235511ea77a9793b263de5fc18b92a3e791b
Amit Karnik <amit.karnik@edgenetworks.in>
2015-07-01 19:05:44 +0530

    work with stack

---
+++ work_stack.py
@@ -0,0 +1,29 @@
+__author__ = 'karnikamit'
+import sys
+from time import sleep
+
+
+def stack():
+    work = ['1. Add to Stack', '2. Pop from stack', '3. Print stack', '4. Exit']
+    stack_list = list()
+    flag = True
+    while flag:
+        for w in work:
+            print w
+        q = int(raw_input('What do you want to do? '))
+        if q == 1:
+            item = str(raw_input("Add "))
+            stack_list.append(item)
+        elif q == 2:
+            stack_list.pop(stack_list.index(str(raw_input('item to pop- '))))
+        elif q == 3:
+            for i in stack_list:
+                print i
+        else:
+            dot = '...'
+            for c in dot:
+                print c,
+                sys.stdout.flush()
+                sleep(1)
+            print 'Bye'
+            flag = False
