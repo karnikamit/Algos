f23ce54aa63d992e1d1a956d1d24ee133eab81a9
karnikamit <amit.karnik@edgenetworks.in>
2015-07-23 12:34:57 +0530

    Optimize required

--- PE/145.py
+++ PE/145.py
@@ -9,23 +9,27 @@ def is_odd(n):
             return True
     return False
 
-# def get_num(n):
-#     for i in xrange(11, n+1):
-#         yield i
+
+def get_num(n):
+    for num in xrange(11, n+1):
+        yield num
+
+
+def check_num(n):
+    n = list(str(n))
+    if all(is_odd(int(num)) for num in n):
+        return True
+    return False
+
 from time import time
 count = 0
-# i = get_num(10)
-# for k in i:
-#     print k
-# exit(0)
 start = time()
-for i in xrange(11, 10**8):
+for i in xrange(11, 10**9):
     k = str(i)
     j = k[::-1]
     if j[0] == '0':
         continue
-    check = list(str(i + int(j)))
-    if all(is_odd(int(num)) for num in check):
+    if check_num(i + int(j)):
         count += 1
 
 print count, '<--count', time()-start, 'seconds'
