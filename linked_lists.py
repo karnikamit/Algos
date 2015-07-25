01f10c517a575d78f8087e2a95ff48125173b79c
karnikamit <amit.karnik@edgenetworks.in>
2015-07-25 11:35:03 +0530

    Linked list

---
+++ linked_lists.py
@@ -0,0 +1,24 @@
+__author__ = 'karnikamit'
+
+
+class Node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+
+    def set_data(self, data):
+        self.data = data
+
+    def get_data(self):
+        return self.data
+
+    def set_next(self, next):
+        self.next = next
+
+    def get_next(self):
+        return self.next
+
+    def has_next(self):
+        if self.next is not None:
+            return True
+        return False
