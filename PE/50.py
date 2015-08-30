1b2311512a84ad72e99bb8ad8f254849340daa5e
karnikamit <amit.karnik@edgenetworks.in>
2015-08-30 18:18:08 +0530

    working 50...

--- PE/50.py
+++ PE/50.py
@@ -3,9 +3,11 @@ from is_prime import is_prime
 
 
 def get_primes(n):
+    primes = []
     for i in xrange(2, n+1):
         if is_prime(i):
-            yield i
+            primes.append(i)
+    return primes
 
 
 def find_con_prime(n):
@@ -17,4 +19,26 @@ def find_con_prime(n):
             if num < n:
                 low = num
     return low
-print find_con_prime(100)
+# print find_con_prime(100)
+
+def get_con_primes(n):
+    con_primes = {}
+    primes = get_primes(n)
+    print primes
+    for num in primes:
+        print 'num', num
+        new_num = 0
+        new_list = []
+        for k in xrange(2, num):
+            new_num += k
+            if new_num == num:
+                pass        # working!
+        # new_primes = primes[:primes.index(num)]
+        # new_num = sum(new_primes)
+        # if new_num > n:
+        #     continue
+        # if is_prime(new_num):
+        #     con_primes[len(new_primes)] = new_num
+    return con_primes
+
+print get_con_primes(1000)
