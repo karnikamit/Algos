d9a8dac0b94f7434b745632bc9172ff1cf2e97b5
Amit Karnik <amit.karnik@edgenetworks.in>
2015-12-17 16:36:06 +0530

    yet to complete!

--- PE/95.py
+++ PE/95.py
@@ -3,25 +3,43 @@ __author__ = 'CYBORG'
 
 
 def sum_proper_divisors(n):
-    i, j = 1, 0
-    while i < n:
-        if n % i == 0:
+    j = 0
+    for i in xrange(1, int(n >> 1)+1):
+        if not n % i:
             j += i
-        i += 1
     return j
 
 
-def find_am_pairs(till):
-    c, no = 0, 0
-    for i in range(till):
-        s = sum_proper_divisors(i)
-        s1 = sum_proper_divisors(s)
-        if s1 == i:
-            r = i - s
-            if r > c:
-                print(i)
-                c = r
-                no = i
-    return no
+def find_am_chain(num, am_list):
+    s = sum_proper_divisors(num)
+    am_list.append(s)
+    if s == am_list[0]:
+        return am_list[:-1]
+    elif s < ori_num:
+        return []
+    return find_am_chain(s, am_list)
 
-print(find_am_pairs(100000))
+num = maximum = 1
+for ori_num in xrange(1, 12497):
+    am_list = list([ori_num])
+    length = len(find_am_chain(ori_num, am_list))
+    (num, maximum) = length > maximum and (ori_num, length) or (num, maximum)
+print num, maximum
+
+
+# for i in xrange(100, 104):
+#     globals()
+#     ori_num = i
+#     print find_am_chain(ori_num)
+
+# def find_longest_am_chain(till):
+#     longest_am_list = []
+#     for i in xrange(till):
+#
+#         temp = find_am_chain(i, i, am_list)
+#         if temp:
+#             if len(temp) > len(longest_am_list):
+#                 longest_am_list = temp
+#     return longest_am_list
+#
+# print find_longest_am_chain(100)
