14059ac5bfc29d245d78a8be054c1ec9308466dd
Amit Karnik <amit.karnik@edgenetworks.in>
2016-01-31 16:54:17 +0530

    Update find missing number function

--- missing_number.py
+++ missing_number.py
@@ -1,27 +1,41 @@
 # -*- coding: utf-8 -*-
 __author__ = "karnikamit"
+import collections
 
 # This is algorithm is for sorted list only.
+# ------------------------========----------
 
 
-def find_missing_number(a_list):
+def find_missing_number(a_list, frequency):
     """
 
     :param a_list: list of numbers
     :return: missing number from the list
     """
-    a_list.sort()
     a_dict = dict()
     for i in xrange(len(a_list)):
         a_dict[i] = a_list[i]
-
     missing_number = None
-
     for key in a_dict:
         if key <= (len(a_list)-1) - 1:
-            if a_dict[key+1] - a_dict[key] != 1:
-                missing_number = a_dict[key+1]-1
+            if a_dict[key+1] - a_dict[key] != frequency:
+                missing_number = a_dict[key+1]-frequency
     return missing_number
 
 if __name__ == '__main__':
-    print find_missing_number([-4, -2, -1])
+    t = [0, 3, 6, 9, 12]
+    t.sort()
+    v = [x[0]-x[1] for x in zip(t[1:], t[:-1])]
+
+    # get the frequency of items
+    counter = collections.Counter(v)
+    fq = max(counter.values())
+    diff = None
+    for key in counter:
+        if counter[key] == fq:
+            diff = key
+    if not all(x == v[0] for x in v):       # check weather an item is missing
+        print 'missing number is %d' % find_missing_number(t, diff)
+    else:
+        print 'No items are missing in %s' % t
+
