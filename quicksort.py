a0de919348d352938c8017dddea9f418280f6322
Amit Karnik <amit.karnik@edgenetworks.in>
2016-02-05 07:00:43 +0530

    Quick sort

--- quicksort.py
+++ quicksort.py
@@ -1,37 +1,56 @@
-__author__ = 'karnikamit'
+# -*- coding: utf-8 -*-
+__author__ = "karnikamit"
+
+'''
+    Quick Sort Algorithm
+
+    1. select a pivot value [usually the first item in the list.    (Assist with splitting the list)
+    2. Partitioning begins by locating two position markers. [leftmost+1, right most]
+        (purpose of partioning is to move items that are on the wrong side with respect to the pivot
+        value while also converging on the split point.)
+    3. Begin by incrementing leftmark until we locate a value that is greater than the pivot value
+    4. Then decrement rightmark until we find a value that is less than the pivot value.
+    5. Now we can exchange these two items and then repeat the process again.
+    6. At the point where rightmark becomes less than leftmark, we stop.
+    7. The position of rightmark is now the split point.
+    8. The pivot value can be exchanged with the contents of the split point
+    9. Repeat
+'''
 
 
-def partition(my_list, start, end):
-    pivot = my_list[start]
-    low = start+1
-    high = end
-    done = False
-    while not done:
-        while low <= high and my_list[low] <= pivot:
-            low += 1
-        while my_list[high] >= pivot and high >= low:
-            high -= 1
-        if high < low:
-            done = True
+def quick_sort(a_list):
+    quick_sort_helper(a_list, 0, len(a_list)-1)
+    return a_list
+
+
+def quick_sort_helper(a_list, first, last):
+    if first < last:
+        split_point = partition(a_list, first, last)
+        quick_sort_helper(a_list, first, split_point-1)
+        quick_sort_helper(a_list, split_point+1, last)
+    return a_list
+
+
+def partition(a_list, first, last):
+    pivot = a_list[first]
+    leftmark = first+1
+    rightmark = last
+    flag = True
+
+    while flag:
+        while leftmark <= rightmark and a_list[leftmark] < pivot:
+            leftmark += 1
+        while a_list[rightmark] > pivot and rightmark >= leftmark:
+            rightmark -= 1
+
+        if rightmark < leftmark:
+            flag = False
         else:
-            my_list[low], my_list[high] = my_list[high], my_list[low]
-    my_list[start], my_list[high] = my_list[high], my_list[start]
-    return high
+            a_list[leftmark], a_list[rightmark] = a_list[rightmark], a_list[leftmark]
+    a_list[first], a_list[rightmark] = a_list[rightmark], a_list[first]
+    return rightmark
 
 
-def quicksort(my_list, start, end):
-    if start < end:
-        split = partition(my_list, start, end)
-        quicksort(my_list, start, split-1)
-        quicksort(my_list, split+1, end)
-    return my_list
-
-
-if __author__ == 'karnikamit':
-    import random
-    import time
-    my_array = range(10**7)
-    random.shuffle(my_array)
-    start_time = time.time()
-    print quicksort(my_array, 0, len(my_array)-1)
-    print time.time()-start_time, " seconds"
+if __name__ == '__main__':
+    a =  [14, 17, 13, 15, 19, 10, 3, 16, 9, 12]
+    print quick_sort(a)
