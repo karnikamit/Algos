df4e83baa010530ed84a901419cd66b6cd15bc9b
Amit Karnik <amit.karnik@edgenetworks.in>
2016-03-29 12:24:37 +0530

    266

--- PE/266.py
+++ PE/266.py
@@ -1,36 +1,40 @@
+# -*- coding: utf-8 -*-
 __author__ = 'karnikamit'
 from is_prime import is_prime
 from numpy import prod
 from math import sqrt
 from time import time
+import itertools
 
 
-def get_prod_prime(n):
-    i = 0
-    primes = []
-    while i < n:
-        if is_prime(i):
-            primes.append(i)
-        i += 1
-    return prod(primes)     # 1456955400340837138 for 190
+def get_prod_primes(n):
+    return reduce(lambda x, y: x*y, [i for i in xrange(1, n) if is_prime(i)])
+
+# TEST
+# def get_raw_psr(n):
+#     check = sqrt(n)
+#     divisors = [i for i in xrange(1, int(check+1)) if n % i == 0]
+#     return max(divisors)
+#
+# print get_raw_psr(3102)
 
 
 def get_psr(n):
-    psr = 0
     check = sqrt(n)
-    for i in xrange(1, int(check+1)):
+    psr_list = []
+    for i in itertools.count(1):
+        if i > int(check):
+            break
         if n % i == 0:
-            if check > i > psr:
-                psr = i
-    return psr
-# print get_psr(3102)   # 47
+            psr_list.append(i)
+    return max(psr_list)
 
 
 def main(limit):
-    prime_prod = get_prod_prime(limit)
-    print 'working now!'
+    prime_prod = get_prod_primes(limit)
     return get_psr(prime_prod) % 10**16
 
-start = time()
-print main(190)
-print time()-start, 'seconds'
+if __name__ == '__main__':
+    start = time()
+    print main(190)
+    print time()-start, 'seconds'
