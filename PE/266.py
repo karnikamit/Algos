4cc3dd086eab8aa091f63603414835ad1eed4141
Amit Karnik <amit.karnik@edgenetworks.in>
2016-03-30 13:58:21 +0530

    266, taking too much of time, Optimize.

--- PE/266.py
+++ PE/266.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'karnikamit'
 from is_prime import is_prime
-from math import sqrt
+from math import sqrt, ceil
 from time import time
 import itertools
 
@@ -10,35 +10,31 @@ def get_prod_primes(n):
     return reduce(lambda x, y: x*y, [i for i in xrange(1, n) if is_prime(i)])
 
 # TEST
+
 # def get_raw_psr(n):
 #     check = sqrt(n)
-#     divisors = [i for i in xrange(1, int(check+1)) if n % i == 0]
-#     return max(divisors)
-#
+#     divisor = [i for i in xrange(1, int(check+1)) if n % i == 0][-1]
+#     return divisor
 # print get_raw_psr(3102)
+# exit(0)
 
 
-def get_divisors(num):
-    n = num/2
-    n += 1
-    return (i for i in itertools.count(1, step=2) if num % i == 0)
+def get_psr(num):
+    print 'getting divisors for %d' % num
+    n, i = sqrt(num), 1
+    divs = 0
+    while i < n:
+        if num % i == 0:
+            divs = i
+        i += 1
+    return divs
 
 
-def get_psr(n):
-    divs = get_divisors(n)
-    check = sqrt(n)
-    old = 0
-    for i in divs:
-        if i > int(check)+1:
-            return old
-        old = i
-
-
-def main(limit):
+def euler(limit):
     prime_prod = get_prod_primes(limit)
     return get_psr(prime_prod) % 10**16
 
 if __name__ == '__main__':
     start = time()
-    print main(190)
+    print euler(50)
     print time()-start, 'seconds'
