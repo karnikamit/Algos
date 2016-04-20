7c3410828bc221a24b8c25832b758ad7a2722279
Amit Karnik <karnikamit46@gmail.com>
2016-04-20 17:01:41 +0530

    PE 37
    RunTime: 3.35 s Optimize it.

---
+++ PE/37.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+__author__ = "karnikamit"
+from get_primes import get_primes
+from is_prime import is_prime
+from time import clock
+
+
+
+def trunc_left(k):
+    should_app = False
+    k_str = str(k)
+    while len(k_str) != 1:
+        k_str = k_str[1:]
+        if is_prime(int(k_str)):
+            continue
+        else:
+            break
+    else:
+        should_app = True
+
+    return should_app
+
+
+def trubnc_right(j):
+    should_app = False
+    j_str = str(j)
+    while len(j_str) != 1:
+        j_str = j_str[:-1]
+        if is_prime(int(j_str)):
+            continue
+        else:
+            break
+    else:
+        should_app = True
+    return should_app
+
+def truncatable_primes(n):
+    primes = get_primes(n)
+    truncatable = []
+    for i in primes:
+        if len(truncatable) == 11:
+            return truncatable
+        prime_string1 = str(i)
+        prime_string2 = str(i)
+        if len(str(i)) > 1:
+            if trunc_left(prime_string1) and trubnc_right(prime_string2):
+                truncatable.append(i)
+
+
+if __name__ == '__main__':
+    start = clock()
+    print sum(truncatable_primes(1000000))
+    print 'RunTime:', clock()-start, 's'
